a09cad6fbaadea41bc8e99675264f47a88bba43f
WYlim1003 <cocolimwanyee@gmail.com>
2026-02-13 19:48:12 +0800

    location service

--- lib/screens/near_me.dart
+++ lib/screens/near_me.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart'; 
 import '../colors/color.dart';
 import '../widgets/bottom_nav.dart';
+import '../services/location_service.dart'; 
 
 class NearMePage extends StatefulWidget {
   const NearMePage({super.key});
@@ -11,10 +13,63 @@ class NearMePage extends StatefulWidget {
 }
 
 class _NearMePageState extends State<NearMePage> {
+  GoogleMapController? _mapController;
+  final LocationService _locationService = LocationService();
+  
+  final TextEditingController _searchController = TextEditingController();
+
   static const CameraPosition _initialPosition = CameraPosition(
-    target: LatLng(3.1209, 101.6538), 
+    target: LatLng(3.1209, 101.6538),
     zoom: 15.0,
   );
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _currentLocation();
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _searchClinics() {
+    String query = _searchController.text;
+    if (query.isNotEmpty) {
+      debugPrint("MediSafe searching for: $query");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Searching for $query...")),
+      );
+    }
+  }
+
+  Future<void> _currentLocation() async {
+    try {
+      Position position = await _locationService.getCurrentLocation();
+      
+      if (_mapController != null) {
+        _mapController!.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 16.0,
+            ),
+          ),
+        );
+      }
+    } catch (e) {
+      debugPrint("Location Error: $e");
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.toString())),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,13 +77,17 @@ class _NearMePageState extends State<NearMePage> {
       backgroundColor: Colors.white,
       body: Stack(
         children: [
-          const GoogleMap(
+          GoogleMap(
             initialCameraPosition: _initialPosition,
-            myLocationEnabled: true,
+            myLocationEnabled: true, 
+            myLocationButtonEnabled: false, 
             zoomControlsEnabled: false,
             mapType: MapType.normal,
+            onMapCreated: (GoogleMapController controller) {
+              _mapController = controller;
+            },
           ),
-
+          
           Positioned(
             top: 60,
             left: 20,
@@ -51,36 +110,51 @@ class _NearMePageState extends State<NearMePage> {
                       ],
                       border: Border.all(color: AppColors.royalBlue),
                     ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        hintText: "Search",
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: const InputDecoration(
+                        hintText: "Search clinics...",
                         border: InputBorder.none,
                         hintStyle: TextStyle(color: Colors.grey),
                       ),
+                      onSubmitted: (_) => _searchClinics(), 
                     ),
                   ),
                 ),
                 const SizedBox(width: 10),
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        blurRadius: 10,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
-                    border: Border.all(color: Colors.black12),
+                GestureDetector(
+                  onTap: _searchClinics, 
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 10,
+                          offset: const Offset(0, 5),
+                        ),
+                      ],
+                      border: Border.all(color: Colors.black12),
+                    ),
+                    child: const Icon(Icons.search, color: Colors.black),
                   ),
-                  child: const Icon(Icons.search, color: Colors.black),
                 ),
               ],
             ),
           ),
+          
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: FloatingActionButton(
+              backgroundColor: Colors.white,
+              onPressed: _currentLocation,
+              child: Icon(Icons.my_location, color: AppColors.royalBlue),
+            ),
+          )
         ],
       ),
       bottomNavigationBar: const MedicalBottomNav(),
